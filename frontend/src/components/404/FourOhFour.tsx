8431b56d8fa454b8450f75ba8b9d31b1c0a872d9
Koushik Roy <96koushikroy@gmail.com>
2024-09-01 19:28:01 -0700

    front end refactor

---
+++ frontend/src/components/404/FourOhFour.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../Layout/Navbar"
+
+export default function FourOhFour(){
+    return (
+        <>
+            <Navbar />
+            <h1>Page Not Found</h1>
+        </>
+    )
+}
